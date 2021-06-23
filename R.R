e2aba4c5f5a6b0a0b59768a3eae18ee89aedaffc
Samira Nassi Celaya <nassi.samira20@gmail.com>
2021-06-22 23:06:14 -0700

    # Need to fix fasta

--- R.R
+++ R.R
@@ -16,7 +16,7 @@ Fusarium_accession <- c("GQ154454", "GQ154455", "GQ154456", "GQ154457",
                         "GQ154462", "GQ154463", "GQ154464", "GQ154465",
                         "GQ154466", "GQ154467", "GQ154468", "GQ154469")
 
-??read.GenBank
+# ?read.GenBank
 
 # read squences and place them in a DNAbin object
 Fusarium_sequences <- read.GenBank(Fusarium_accession)
@@ -33,5 +33,21 @@ attr(Fusarium_sequences, "species")
 write.dna(Fusarium_sequences, file = "Fusarium_fasta_1.fasta", format = "fasta",
           append = FALSE, nbcol = 6, colsep = " ", colw = 10)
 
-#
-path <- file.path(getwd(), )
+###To make accession numbers into species name, but all named Fusarium_oxysporum??? 
+###Fusarium_sequences_GenBank_IDs <- paste(attr(Fusarium_sequences, "species"), names
+###(Fusarium_sequences), sep ="_RAG1_") 
+
+###Fusarium_16 <- read.fasta(file = "Fusarium_fasta_1.fasta", seqtype = "DNA",
+###                          as.string = TRUE, forceDNAtolower = FALSE)
+
+###write.fasta(sequences = Fusarium_16, names = Fusarium_sequences_GenBank_IDs,
+###            nbchar = 10, file.out = "Fusarium_seq_seqinr_format.fasta")
+
+# path for downloaded file to R 
+path <- file.path(getwd(), "Consensus-22Jun2021.fasta")
+
+data.frame("Consensus-22Jun2021.fasta")
+
+Fusarium_fasta_1 <- read.table(file = path, sep = '\t', header = FALSE)
+
+duplicated(Fusarium_fasta_1.fasta)
