99f220020b95da22a56e429b9e5a5836c2056676
Samira Nassi Celaya <nassi.samira20@gmail.com>
2021-07-03 12:25:25 -0700

    # With correct accessions now

--- R.R
+++ R.R
@@ -1,5 +1,5 @@
 #Importing GenBank data---------------------------------------------------------
-  
+
 # R package for phylogenentics and comparative methods
 #install.packages("ape")
 library(ape)
@@ -9,62 +9,63 @@ library(ape)
 library(seqinr)
 
 # Creating a vector of GenBank accession numbers we want
-Fusarium_accession <- c("GQ154454", "GQ154455", "GQ154456", "GQ154457",
-                        "GQ154458", "GQ154459", "GQ154460", "GQ154461",
-                        "GQ154462", "GQ154463", "GQ154464", "GQ154465",
-                        "GQ154466", "GQ154467", "GQ154468", "GQ154469")
+Fusarium_accession <- c("AY320087", "GU170559", "FJ985328", "AY527534",
+                        "AF008506", "FJ985326", "KX434896", "FJ985388",
+                        "FJ985365", "AF008485", "AF008491", "FJ985412", 
+                        "FJ985331", "FJ985312", "AF008490", "FJ985270",
+                        "FJ985395", "FJ985330", "FJ985300",
+                        "AF008509", "FJ985407", "MF684761", "GQ154455",
+                        "GQ154462", "GQ154458", "GQ154456", "GQ154460",
+                        "GQ154461", "GQ154454", "FJ985337")
 
 # Reads squences and place them in a DNAbin object
-Fusarium_16 <- read.GenBank(Fusarium_accession)
+Fusarium_fasta <- read.GenBank(Fusarium_accession)
 
 # List of DNAbin objects
-str(Fusarium_16)
+str(Fusarium_fasta)
 
 # Species list
-attr(Fusarium_16, "species")
+attr(Fusarium_fasta, "species")
 
 # Fatsa file format imported to R project
-write.dna(Fusarium_16, file = "Fusarium_16.fasta", format = "fasta",
-          append = FALSE, nbcol = 1, colsep = " ", colw = 80)
+write.dna(Fusarium_fasta, file = "Fusarium_fasta.fasta", format = "fasta",
+          append = FALSE, nbcol = 6, colsep = " ", colw = 10)
 
 # Formatting Consensus----------------------------------------------------------
 
-### Make sure to download and move consensus file to R project folder!!!! ###
+### Make sure to download and move consensus file to R project folder     ###
+### or documents!!!!                                                      ###
 
-# path for downloaded file to R
-path <- file.path(getwd(), "Consensus-22Jun2021.fasta")
-#path2 <- system.file(path)
+# Path for downloaded file to R
+path <- file.path(getwd(), "EC_006_S3-2_ITS4_B06.ab1.gb")
 
-# to make a new file of consensus lowercase
+# To make a new file of consensus lowercase
 Fusarium_1 <- readLines(path)
 Fusarium_1_lower <- tolower(Fusarium_1)
 # to save lower case consensus
-writeLines(Fusarium_1_lower, "consensus_lower.fasta")
+writeLines(Fusarium_1_lower, "EC_lower")
 
-### I copy pasted consensus_lower.fasta to the bottom of Fusarium_16.fasta ###
-###                     and saved it!!!                                    ###
+### I copy pasted consensus_lower.fasta to the bottom of Fusarium_fasta    ###
+###                      and saved it!!!                                   ###
 
 # Multiple sequencing using msa-------------------------------------------------
 
-#if (!requireNamespace("BiocManager", quietly = TRUE))
-#  install.packages("BiocManager")
-#BiocManager::install("msa")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("msa")
 library(msa)
 
 #Downloading fasta file already downloaded in desktop with Biostrings
 #BiocManager::install("Biostrings")
 library(Biostrings)
 
-# to locate texshade file
-#system.file("tex", "texshade.sty", package = "msa")
-
-path2 <- file.path(getwd(), "Fusarium_16.fasta")
+path2 <- file.path(getwd(), "Fusarium_fasta.fasta")
 
 Fusarium_sequences <- readAAStringSet(path2)
 
 Fusarium_sequences
 
-Fusarium_alignment <- msa(Fusarium_sequences)
+Fusarium_alignment<- msa(Fusarium_sequences)
 
 #Or
 
@@ -77,3 +78,23 @@ Fusarium_alignment <- msa(Fusarium_sequences, "Muscle") # WORKED???????????????
 Fusarium_alignment
 
 print(Fusarium_alignment, show = "complete")
+
+# To install tinytex to use msaPrettyPrint function
+install.packages("tinytex")
+library(tinytex)
+install.packages("knitr")
+library(knitr)
+# to locate texshade file
+p <-system.file("tex", "texshade.sty", package = "msa")
+
+msaPrettyPrint(Fusarium_alignment, output = "pdf", showNames = "none",
+               showLogo = "none", askForOverwrite = FALSE)
+texify.exe("Fusarium_alignment.tex", clean=TRUE)
+
+texi2pdf("Fusarium_alignment.tex", clean = FALSE, quiet = TRUE,
+         texi2dvi = getOption("texi2dvi"),
+         texinputs = NULL, index = TRUE)
+
+path5
+
+Sys.setenv(PATH = paste(Sys.getenv("PATH"), "C:/Program Files/MiKTeX 2.9/miktex/bin/x64", sep=.Platform$path.sep))
